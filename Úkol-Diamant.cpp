df48f1090102ca1dbfe36bc90c7270a93cec5a42
stepaaahoris <92727466+stepaaahoris@users.noreply.github.com>
2021-12-19 18:43:53 +0100

    Add files via upload

---
+++ Úkol-Diamant.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+
+int main(void) {
+
+	int a = 10;
+	for (int j = 0; j <= 9; j++) {
+		for (int i = 0; i < a; i++) {
+			printf(" ");
+		}
+		for (int y = j; y >= 1; y--) {
+			printf("%d", y);
+		}
+		for (int y = 2; y <= j; y++) {
+			printf("%d", y);
+		}
+		printf("\n");
+		a--;
+	}
+	a = 2;
+	for (int j = 8; j >= 0; j--) {
+		for (int i = 0; i < a; i++) {
+			printf(" ");
+		}
+		for (int y = j; y >= 1; y--) {
+			printf("%d", y);
+		}
+		for (int y = 2; y <= j; y++) {
+			printf("%d", y);
+		}
+		printf("\n");
+		a++;
+	}
+}
